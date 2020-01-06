671818d89aab73db5385eb46e46e1b723f8acf82
anguspettifer <angus.pettifer@gmail.com>
2020-01-06 09:05:36 +0000

    test number of humans

--- test/zombie_invasion/test_game.py
+++ test/zombie_invasion/test_game.py
@@ -7,10 +7,23 @@ from zombie_invasion.game import Game
 @patch('zombie_invasion.game.input')
 def test_game_request_dimensions(mock_input, mock_print):
     """
-    Check that starting a game asks the user for dimensions as an input and saves response
+    Check that requesting the user for dimensions as an input saves response
     """
     mock_input.return_value = (20, 40)
     game = Game()
-    game.start()
+    game._request_dimensions()
     mock_print.assert_called_with("Please enter dimensions")
     assert game.dimensions == (20, 40)
+
+
+@patch('zombie_invasion.game.print')
+@patch('zombie_invasion.game.input')
+def test_game_request_dimensions(mock_input, mock_print):
+    """
+    Check that requesting the user for number of humans as an input saves response
+    """
+    mock_input.return_value = 5
+    game = Game()
+    game._request_number_of_humans()
+    mock_print.assert_called_with("Please enter number of humans")
+    assert game.number_of_humans == 5
--- zombie_invasion/game.py
+++ zombie_invasion/game.py
@@ -2,7 +2,16 @@ class Game:
 
     def __init__(self):
         self.dimensions = None
+        self.number_of_humans = None
 
-    def start(self):
+    def _request_dimensions(self):
         print("Please enter dimensions")
         self.dimensions = input()
+
+    def _request_number_of_humans(self):
+        print("Please enter number of humans")
+        self.number_of_humans = input()
+
+    def start(self):
+        # self._request_dimensions()
+        print("not tested")
